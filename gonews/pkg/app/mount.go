c8200fae42f52b91db3d57fa9d7f1e0cffe95376
korn <kornchaval@gmail.com>
2017-08-28 21:15:16 +0700

    first commit

---
+++ gonews/pkg/app/mount.go
@@ -0,0 +1,23 @@
+package app
+
+import "net/http"
+
+// Mount mount handler to mux (ชื่อ func ที่เป็นตัวใหญ่นำหน้าจะมีการ Export จำเป็นต้องมีการ commnet)
+func Mount(mux *http.ServeMux) {
+	mux.HandleFunc("/", index)         // list all news
+	mux.HandleFunc("/news/", newsView) // /news/:path
+
+	adminMux := http.NewServeMux()
+	adminMux.HandleFunc("/login", adminLogin)   // /admin/login
+	adminMux.HandleFunc("/list", adminList)     // /admin/list
+	adminMux.HandleFunc("/create", adminCreate) // /admin/create
+	adminMux.HandleFunc("/edit", adminEdit)     // /admin/edit
+
+	mux.Handle("/admin/", onlyAdmin(adminMux))
+}
+
+//Middleware สำหรับ check การ Login ของ Admin
+
+func onlyAdmin(h http.Handler) http.Handler {
+	return h
+}
