9bb8912f25862359435659604fe504348e9d2095
korn <kornchaval@gmail.com>
2017-09-12 15:55:28 +0700

    connect MongoServer

--- gonews/cmd/gonews/config.go
+++ gonews/cmd/gonews/config.go
@@ -1,3 +1,3 @@
 package main
 
-const port = ":8080"
+//const port = ":8080"
--- gonews/cmd/gonews/main.go
+++ gonews/cmd/gonews/main.go
@@ -1,30 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/subalgo/gonews/pkg/model"
 
 	"github.com/subalgo/gonews/pkg/app"
 )
 
-const (
-	//port     = ":8080" //port ดึงมาจาก file config.go แล้ว
-	mongoURL = "mongodb://127.0.0.1:27017"
-)
-
-//const port = ":8080"
-
 func main() {
+	var config struct {
+		MongoURL string `json:"mongo_url"`
+	}
+	b, err := ioutil.ReadFile("config.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(b, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	mux := http.NewServeMux()
 	app.Mount(mux)
-	err := model.Init(mongoURL)
+	err = model.Init(config.MongoURL)
 	if err != nil {
 		log.Fatalf("can not init model: %v", err)
 	}
 	log.Println(port)
-	http.ListenAndServe(port, mux)
+	http.ListenAndServe(":"+port, mux)
 
 	/*
 		mux := http.NewServeMux()
